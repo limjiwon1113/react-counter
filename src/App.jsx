e108ebf772c4f041aef1d762c99d893db12a3790
jiwon <limjiwon1113@etribe.co.kr>
2025-01-08 11:45:46 +0900

    카운터 종료

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import "./App.scss";
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
+import Even from "./components/Even";
+
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  const isMount = useRef(false);
+
+  // 1. 마운트 : 탄생
+  useEffect(()=>{
+    console.log("마운트")
+  },[]);
+
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(()=>{
+    if(!isMount.current){
+      isMount.current = true;
+      return;
+    }
+    console.log("업데이트");
+  });
+
+  // 3. 언마운트 : 죽음
+  
+
+  useEffect(()=>{
+    console.log({count, input});
+  }, [count, input])
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -13,9 +39,15 @@ function App() {
   return (
     <div className="counter-wrap">
       <h1>Simple Counter</h1>
+      <seciont>
+        <input value={input} onChange={(e)=>{
+          setInput(e.target.value);
+        }}></input>
+      </seciont>
 
       <section>
         <Viewer count={count}/>
+        {count % 2 === 0 ? <Even/> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton}/>
